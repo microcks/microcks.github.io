fcedc607e2e286056e26b56f76980c830bdc52d3
Laurent Broudoux <laurent.broudoux@gmail.com>
2019-11-27 08:57:58 +0100

    New website publication

---
+++ themes/microcks/assets/js/docsearch.js
@@ -0,0 +1,8 @@
+var docsearch = require('docsearch.js/dist/cdn/docsearch.js');
+docsearch({
+  appId: 'HAOH56NU0L',
+  apiKey: '813d24f4234329f165a773a9fc0e5011',
+  indexName: 'microcks.io',
+  inputSelector: '#search-input',
+  debug: true // Set debug to true if you want to inspect the dropdown
+});
